4178510c172aed30bc8211c49e730a3c5f271838
romeova <romeova.7243@gmail.com>
2019-04-23 22:42:31 -0500

    Update control flow

--- app/src/main/java/mx/itesm/csf/deacero/Login.java
+++ app/src/main/java/mx/itesm/csf/deacero/Login.java
@@ -1,7 +1,9 @@
 package mx.itesm.csf.deacero;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void irAMenu(View v){
+        startActivity(new Intent(this, Menu_Graficas.class));
     }
 }
